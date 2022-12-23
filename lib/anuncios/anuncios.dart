99105aba9e8db2eda402b202d8f30d1ab4ce3b71
marisset <116570053+marisset@users.noreply.github.com>
2022-12-23 01:04:48 -0500

    Revert "anuncio intersticial"
    This reverts commit ddd6f811d78ae98ad4283530ee6b277dbc239300.

--- lib/anuncios/anuncios.dart
+++ lib/anuncios/anuncios.dart
@@ -1,36 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
-
 
 class AnunciosDemo extends StatelessWidget {
-   AnunciosDemo({Key? key}) : super(key: key) {
-    _initAd();
-  }
-
-  late InterstitialAd _interstitialAd;
-  bool _isAdLoaded = false;
-
-  void _initAd() {
-    var testAdUnitId;
-    InterstitialAd.load(
-      adUnitId: 'ca-app-pub-3940256099942544/1033173712', 
-      request: AdRequest(), 
-      adLoadCallback: InterstitialAdLoadCallback(
-        onAdLoaded: onAdLoaded, 
-        onAdFailedToLoad: (error) {}, ), ); 
-  }
-
-  void onAdLoaded(InterstitialAd ad){
-    _interstitialAd = ad;
-    _isAdLoaded = true;
-
-    _interstitialAd.fullScreenContentCallback = FullScreenContentCallback();
-    onAdDismissedFullScreenContent: (ad){
-      _interstitialAd.dispose();
-    };
-    
-  
-  }
+  const AnunciosDemo({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -38,19 +9,12 @@ class AnunciosDemo extends StatelessWidget {
       appBar: AppBar(
         centerTitle: true,
         elevation: 0,
-        title: const Text('ANUNCIOS'),
+        title: const Text('Tu frase del día es...'),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            if(_isAdLoaded){
-              _interstitialAd.show();
-            }
-          }, 
-          child: Text ('Ver Anuncio'),
+      body: Container(
+        alignment: Alignment.center,
+        child: const Text('Hola!'),
       ),
-    ),);
+    );
   }
-  
-  
 }
