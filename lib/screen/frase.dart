c1c39c58a8464b11f7b38e0d7c77c8c596d4cb1f
Paola CF <116570902+pao3cf@users.noreply.github.com>
2022-12-23 01:52:36 -0500

    frases

--- lib/screen/frase.dart
+++ lib/screen/frase.dart
@@ -1,19 +1,48 @@
 import 'package:flutter/material.dart';
 
-class FraseScreen extends StatelessWidget {
+class FraseScreen extends StatefulWidget {
   const FraseScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FraseScreen> createState() => _FraseScreenState();
+}
+
+class _FraseScreenState extends State<FraseScreen> {
+  // Frases
+  List<String> list = [
+    'El camino al éxito, es la actitud',
+    'No busques los errores, busca un remedio',
+    'Si te caíste ayer, levántate hoy',
+    'La vida es una aventura, atrévete ',
+    'El mejor momento del día es ahora',
+    'El valor de una idea radica en su uso',
+    'Haz de cada día tu obra maestra',
+    'Si la oportunidad no llama, construye una puerta',
+    'No cuentes los días, haz que los días cuenten',
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
-        elevation: 0,
-        title: const Text('Tu frase del día es...'),
+        title: const Text('Frases del día'),
       ),
-      body: Container(
-        alignment: Alignment.center,
-        child: const Text('Hola!'),
+      body: ReorderableListView.builder(
+        itemBuilder: (context, index) {
+          return ListTile(
+            key: ValueKey(list[index]),
+            title: Text(list[index]),
+          );
+        },
+        itemCount: list.length,
+        onReorder: (oldIndex, newIndex) {
+          //list re-arrange
+          if (oldIndex < newIndex) {
+            newIndex--;
+          }
+          final item = list.removeAt(oldIndex);
+          list.insert(newIndex, item);
+        },
       ),
     );
   }
