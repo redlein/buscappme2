53e047d4be42c192b3e11882b70ec654f6bf4458
Paola CF <116570902+pao3cf@users.noreply.github.com>
2023-01-10 12:13:16 -0500

    diseño de contactanos

--- lib/Pages/contactoscreen.dart
+++ lib/Pages/contactoscreen.dart
@@ -37,13 +37,13 @@ class _contactoScreenState extends State<contactoScreen> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Padding(
-                        padding: const EdgeInsets.all(10.0),
+                        padding: const EdgeInsets.all(6.0),
                         child: Container(
                           child: SizedBox(
                               child: ClipRRect(
                             child: Image.asset(
                               'assets/logo.png',
-                              width: 300,
+                              width: 260,
                             ),
                           )),
                         ),
@@ -58,25 +58,44 @@ class _contactoScreenState extends State<contactoScreen> {
                           ),
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: const Text(
-                          '927478925',
-                          style: TextStyle(
-                            fontSize: 17,
-                            // color: Colors.black,
-                          ),
+                      Container(
+                        width: 270,
+                        height: 80,
+                        alignment: Alignment.bottomCenter,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.rectangle,
+                          color: Theme.of(context).backgroundColor,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(6.0),
+                              child: const Text(
+                                '927478925',
+                                textAlign: TextAlign.justify,
+                                style: TextStyle(
+                                  fontSize: 17,
+                                  color: Colors.orange,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(6.0),
+                              child: const Text(
+                                'buscappme@gmail.com',
+                                textAlign: TextAlign.justify,
+                                style: TextStyle(
+                                  fontSize: 17,
+                                  color: Colors.orange,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: const Text(
-                          'buscappme@gmail.com',
-                          style: TextStyle(
-                            fontSize: 17,
-                            // color: Colors.black,
-                          ),
-                        ),
+                      const SizedBox(
+                        height: 5,
                       ),
                       Visibility(
                         visible: launcherProvider.isVisible,
@@ -85,9 +104,9 @@ class _contactoScreenState extends State<contactoScreen> {
                           child: Container(
                             decoration: BoxDecoration(
                               borderRadius: BorderRadius.circular(20),
-                              color: Colors.white.withOpacity(1),
+                              color: Theme.of(context).backgroundColor,
                             ),
-                            width: MediaQuery.of(context).size.width * 0.8,
+                            width: MediaQuery.of(context).size.width * 0.7,
                             height: MediaQuery.of(context).size.height * 0.1,
                             child: Row(
                                 mainAxisAlignment:
@@ -99,6 +118,7 @@ class _contactoScreenState extends State<contactoScreen> {
                                       onPressed: () {
                                         launcherProvider.goEmailLauncher();
                                       },
+                                      heroTag: "email",
                                       backgroundColor: Colors.orange,
                                       child: const Icon(Icons.email),
                                     ),
@@ -109,6 +129,7 @@ class _contactoScreenState extends State<contactoScreen> {
                                       onPressed: () {
                                         launcherProvider.goPhoneLauncher();
                                       },
+                                      heroTag: "phone",
                                       backgroundColor: Colors.cyan,
                                       child: const Icon(Icons.phone),
                                     ),
@@ -119,6 +140,7 @@ class _contactoScreenState extends State<contactoScreen> {
                                       onPressed: () {
                                         launcherProvider.goWhatsappLauncher();
                                       },
+                                      heroTag: "wsp",
                                       backgroundColor: Colors.green,
                                       child:
                                           const Icon(Icons.whatsapp_outlined),
@@ -128,112 +150,130 @@ class _contactoScreenState extends State<contactoScreen> {
                           ),
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: const [
-                                Text(
-                                  'Denuncia: ',
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    color: Colors.red,
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Container(
+                        width: 270,
+                        height: 140,
+                        alignment: Alignment.bottomRight,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.rectangle,
+                          color: Theme.of(context).backgroundColor,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: const [
+                                  Text(
+                                    'Denuncia: ',
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.red,
+                                      fontWeight: FontWeight.bold,
+                                    ),
                                   ),
-                                ),
-                                Text(
-                                  'Doc. policial',
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                    // color: Colors.black,
+                                  Text(
+                                    'Doc. policial',
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      // fontWeight: FontWeight.bold,
+                                      color: Colors.lightBlue,
+                                    ),
                                   ),
-                                ),
-                              ],
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: const [
-                                Text(
-                                  'Datos: ',
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    color: Colors.red,
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: const [
+                                  Text(
+                                    'Datos: ',
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.red,
+                                      fontWeight: FontWeight.bold,
+                                    ),
                                   ),
-                                ),
-                                Text(
-                                  'Foto de DNI/P.Nacimiento',
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                    // color: Colors.black,
+                                  Text(
+                                    'Foto de DNI/P.Nacimiento',
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      // fontWeight: FontWeight.bold,
+                                      color: Colors.lightBlue,
+                                    ),
                                   ),
-                                ),
-                              ],
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: const [
-                                Text(
-                                  'Fotos: ',
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    color: Colors.red,
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: const [
+                                  Text(
+                                    'Fotos: ',
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.red,
+                                      fontWeight: FontWeight.bold,
+                                    ),
                                   ),
-                                ),
-                                Text(
-                                  '2 a 3 formato png/pjg',
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                    // color: Colors.black,
+                                  Text(
+                                    '2 a 3 formato png/pjg',
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      // fontWeight: FontWeight.bold,
+                                      color: Colors.lightBlue,
+                                    ),
                                   ),
-                                ),
-                              ],
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: const [
-                                Text(
-                                  'Contacto: ',
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    color: Colors.red,
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: const [
+                                  Text(
+                                    'Contacto: ',
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.red,
+                                      fontWeight: FontWeight.bold,
+                                    ),
                                   ),
-                                ),
-                                Text(
-                                  'Número de celular',
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                    // color: Colors.black,
+                                  Text(
+                                    'Número de celular',
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      // fontWeight: FontWeight.bold,
+                                      color: Colors.lightBlue,
+                                    ),
                                   ),
-                                ),
-                              ],
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: const [
-                                Text(
-                                  'Correo: ',
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    color: Colors.red,
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: const [
+                                  Text(
+                                    'Correo: ',
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.red,
+                                    ),
                                   ),
-                                ),
-                                Text(
-                                  'gmail u otros',
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
+                                  Text(
+                                    'gmail u otros',
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.lightBlue,
+                                    ),
                                   ),
-                                ),
-                              ],
-                            ),
-                          ],
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
                       )
                     ],
